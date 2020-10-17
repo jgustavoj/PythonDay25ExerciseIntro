aeb676b01a6ebca63dd19fd0c9eab36cc54facc3
jgustavoj <gjime011@fiu.edu>
2020-10-17 01:02:25 +0000

    done

--- exercises/21-The-Beatles/app.py
+++ exercises/21-The-Beatles/app.py
@@ -1,5 +1,15 @@
 # Your code here!!
-
+def sing():
+    for i in range(0,12):
+        if i == 4:
+            print("whisper words of wisdom,")
+        elif i == 10:
+            print("there will be an answer,")
+        elif i == 11:
+            print("let it be")
+        else:
+            print("let it be,")
+sing()
 
 
 
---
+++ exercises/22-Bottles-Of-Milk/app.py
@@ -0,0 +1,9 @@
+def number_of_bottles():
+    for i in reversed(range(0,100)):
+        if i == 1:
+            print(str(i) + str(" bottle of milk on the wall, 1 bottle of milk. Take one down and pass it around, no more bottles of milk on the wall."))
+        elif i == 0:
+            print("No more bottles of milk on the wall, no more bottles of milk. Go to the store and buy some more, 99 bottles of milk on the wall.")
+        else:
+            print(str(f"{i} bottles of milk on the wall, {i} bottles of milk. Take one down and pass it around, {i-1} bottles of milk on the wall."))
+number_of_bottles()
